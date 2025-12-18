0767476d50008e455dbb56c7f78f634d2bbe6222
Bahodir2022 <omonqulovjasur0903@gmail.com>
2025-12-18 14:46:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 import sys,os
-// mk kalta
+# mk kalta
 from aiogram import Bot, Dispatcher, html, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
@@ -78,4 +78,5 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
     asyncio.run(main())
+
 
